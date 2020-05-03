3ee8357a8018e9cdad437dbc57845e37bad071dc
prestlhh <prestlhh@126.com>
2020-05-03 12:38:10 +0800

    add a method

--- src/main/java/com/huize/example/Example.java
+++ src/main/java/com/huize/example/Example.java
@@ -3,6 +3,13 @@ package com.huize.example;
 public class Example{
 	
 	public static void main(String[] args){
+		int a = 6;
+		int b = 6;
 		System.out.println("hello world!");
+		System.out.println(a + "+" + b + "=" + add(a, b));
+	}
+	
+	public static add(int a, int b){
+		return a + b;	
 	}
 }
